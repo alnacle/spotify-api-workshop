dc86bb5815b311d90cbfecfecc595bfeea08d432
Alvaro Navarro <alvaron@spotify.com>
2022-01-24 12:05:22 +0100

    Add authorization

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 
@@ -7,13 +8,68 @@ app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
+const redirect_uri = "http://localhost:3000/callback"
+const client_id = "9f804d705d02479aa6f6caf1c69795d2"
+const client_secret = "167057a4f9df4a77a84e9af0794edbde"
+
+global.access_token;
+
 app.get("/", function (req, res) {
   res.render("index");
 });
 
 app.get("/authorize", (req, res) => {
-  console.log("authorize");
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: client_id,
+    scope: "",
+    redirect_uri: redirect_uri
+  })
+
+  res.redirect("https://accounts.spotify.com/authorize?" + auth_query_parameters.toString());
+
 });
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code;
+  
+  var body = new URLSearchParams({
+    code: code,
+    redirect_uri : redirect_uri,
+    grant_type: "authorization_code"
+  })
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    body: body,
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization:
+      "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64")
+    }
+  })
+
+  const data = await response.json();
+  global.access_token = data.access_token;
+
+  res.redirect("/dashboard")
+});
+
+app.get("/dashboard", async (req, res) => {
+
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + global.access_token
+    }
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  res.render("dashboard", { user: data })
+});
+
 
 let listener = app.listen(3000, function () {
   console.log(
